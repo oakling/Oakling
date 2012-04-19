d026d3c6f9edd83a36407eb5e10415ce8dd16649
Robert Clancy <robclancy@gmail.com>
2012-04-19 19:50:37 +0100

    Revert "Old changes"
    This reverts commit b00cc17ac00d69158096b8bb9b8868de187a708d.

--- lib/scrapers/journals/utils.py
+++ lib/scrapers/journals/utils.py
@@ -1,22 +1,15 @@
+from lib.scrapers.journals import scrape_pr
 from lib.utils import save
-import urls
-import re
-
-def scrape(url):
-    for pattern, scraper in urls.urls:
-        if re.search(pattern, url):
-            if not hasattr(scraper, 'scrape'):
-                scraper = __import__(scraper, fromlist=['scrape'])
-            return scraper.scrape(url)
-    # raise an error here
 
 def scrape_and_add(url):
     """Scrape the journal page and add to database."""
 
-    article = scrape(url)
+    #Find the appropriate scraper
+    #Call it
+    article = scrape_pr.scrape(url)
     #Add entry to database
     return save(article)
 
-def link(new_id, old_ids):
-    """Create links between entries."""
+def merge(new_id, old_ids):
+    """Try to merge the two database entries."""
     pass
