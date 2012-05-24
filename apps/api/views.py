78bb2006cddf4aa01aed11492eb4adcb523620c9
Tim Green <timothy.green@gmail.com>
2012-05-24 22:46:30 +0100

    Filtering journal api command

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
+import re
 import json
 
 from django.template import RequestContext
@@ -19,9 +20,14 @@ def latest(request, num):
 
   return HttpResponse(json.dumps([row.doc for row in rows]), content_type='application/json')
 
+def clean_journal(s):
+  # keep only alphanumeric characters for comparison purposes
+  return re.sub('\s+', ' ', re.sub('[^a-z]', ' ', s.lower()))
+
 def journals(request):
   db = couchdb.Server()['store']
+  filter = clean_journal(request.GET.get('filter', None))
 
   rows = db.view('index/journals', group=True)
 
-  return HttpResponse(json.dumps([row.key for row in rows]), content_type='application/json')
+  return HttpResponse(json.dumps([row.key for row in rows if filter is None or filter in clean_journal(row.key)]), content_type='application/json')
