2850fa265095a8793f28783643a0c3772bc8df87
Craig Loftus <craigloftus+github@gmail.com>
2012-05-27 11:49:16 +0100

    Adding support for auto-completing of Journals and changes to formatting of articles

---
+++ apps/search/static/search/js/main.js
@@ -0,0 +1,59 @@
+var akorn = {
+    make_article_item: function(doc) {
+        // TODO Use a template on the server
+        var abstract = '';
+        // Parse the date
+        var published = new Date(doc.date_published*1000);
+
+        // TODO Make abstract shortening not stupid
+        if(doc.abstract !== undefined) {
+            abstract = ['<p class="abstract">',doc.abstract.substr(0,300),'...</p>'].join('');
+        }
+        return ['<li><h3><a href="',doc.ids.url,'">',doc['title'],
+        '</a></h3><p><span class="authors">',
+        doc.author_names.slice(0,3).join(', '),
+        '</span> - <time datetime="',
+        published.toISOString(),
+        '" class="published">',
+        published,
+        '</time> in <span class="journal"><a href="#">',
+        doc.citation.journal,
+        '</a></span></p>',
+        abstract,
+        '</li>'].join('');
+    },
+    refresh_articles: function(data) {
+        var article_len = data.length;
+        var articles = $('#latest_articles');
+        var doc, i;
+        var fragment = [];
+
+        // Process each article
+        for(i=0; i < article_len; i += 1) {
+            doc = data[i];
+            fragment.push(akorn.make_article_item(doc));
+        }
+        // Add in one go as fragment
+        articles.append(fragment.join(''));
+        // TODO Should be done in akorn.make_article_item
+        // Convert abs. times to relative
+        articles.find('time').relativeTime();
+    },
+    init: function() {
+        // TODO Get cached HTML fragment
+        // Get initial articles
+        $.get('/api/latest/100', {}, akorn.refresh_articles, 'json');
+
+        // TODO Search input should be hidden before this point
+        // Activate the search box
+        $('#search').tagsInput({
+            autocomplete_url:'/api/journals',
+            autocomplete:{selectFirst:true,width:'100px',autoFill:true},
+            height: '15px',
+            width: '800px',
+            defaultText: 'Search',
+        });
+    },
+};
+
+$(document).ready(akorn.init);
