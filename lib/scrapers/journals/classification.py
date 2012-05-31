e8d8f1773186055ecce8359a65699ee38f0e72c8
Robert Clancy <robclancy@gmail.com>
2012-05-31 16:07:51 +0100

    Changed classification scheme for arxiv.

--- lib/scrapers/journals/classification.py
+++ lib/scrapers/journals/classification.py
@@ -1,5 +1,6 @@
 codes = [
-    (r'[a-zA-Z-]+\.[a-zA-Z-]+', 'arxiv'),
+    (r'(?:astro-ph|cond-mat|cs|gr-qc|hep-ex|hep-lat|hep-ph|hep-th|math|math-ph|nlin|nucl-ex|nucl-th|physics|q-bio|q-fin|quant-ph|stat)(?:\.[a-zA-Z-]+)*',
+     'arxiv'),
     (r'[0-9]{2,2}[A-Z][0-9]{2,2}', 'MSC'),
     (r'[A-Z](?:\.[0-9m]){1,2}', 'ACM'),
 ]
