b97f4ca48fdaf81c44511829437185b538619256
Craig Loftus <craigloftus@gmail.com>
2012-06-09 22:16:08 +0100

    Making some styling changes, switching out the autocompletion plugin and adding support for querying based on the search input

--- apps/search/static/search/js/main.js
+++ apps/search/static/search/js/main.js
@@ -1,60 +1,107 @@
 // Namespace for akorn js
 var akorn = {
+    // Storing set nice names for months
     // TODO Really!? In this century?
     month_names: [ "January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December" ],
+    // Append a given HTML Fragment to the articles list
     append_articles: function(data) {
         // Add in one go as fragment
         akorn.articles_container.append(data);
         // Add date lines
-        if(akorn.prev_article !== undefined) {
-            akorn.add_date_lines();
-        }
+        akorn.add_date_lines();
         // Once we have finished adding, stop the pause on updates
+        akorn.unpause_updates();
+    },
+    // Replaces the articles with a new set, rather than appending
+    replace_articles: function(data) {
+        // Remove prev article state
+        akorn.prev_article = undefined;
+        // Replace the current articles with new ones
+        akorn.articles_container.html(data);
+        // Add date lines
+        akorn.add_date_lines();
+        // Once we have finished adding, stop the pause on updates
+        akorn.unpause_updates();
+    },
+    // Stop the scroll updates
+    unpause_updates: function() {
         // Stick a delay in to mitigate scrollbar glitches
         window.setTimeout('akorn.pause_updates = false', 400);
     },
-    get_articles: function(num, article_id) {
-        // Get a specified number of articles
+    // Get a specified number of articles
+    get_articles: function(num, article_id, query, clear) {
         // Optionally pass an article id as the 2nd argument
         //      Function will return the articles after this article id
+        // Optionally pass a query string as the 3rd argument
+        //      Function will filter articles based on the query
+        // Optionally pass a flag as the 4th argument
+        //      Function will switch from appending to replacing articles
         var params = {};
+        var callback;
+
         if(article_id !== undefined) {
             params['article'] = article_id;
         }
-        $.get('/api/latest/'+num, params, akorn.append_articles, 'html');
+        if(query !== undefined) {
+            params['q'] = query;
+        }
+        if(clear !== undefined) {
+            callback = akorn.replace_articles;
+        }
+        else {
+            callback = akorn.append_articles;
+        }
+        $.get('/api/latest/'+num, params, callback, 'html');
     },
+    // Add the next chunk of articles for the current query
     add_more_articles: function() {
             // Get the id of the last article
             var last_article = akorn.articles_container.find('li:last-child');
             // Stash this to use when adding date lines
             akorn.prev_article = last_article;
+            // Get the current query, if set
+            var query = akorn.query;
             // Get articles after the current last article
-            akorn.get_articles(20, last_article.attr('id'));
+            akorn.get_articles(20, last_article.attr('id'), query);
     },
+    // Cycle through the articles marking date changes
     add_date_lines: function() {
         var prev_article = akorn.prev_article;
+        if(prev_article === undefined) {
+            // If it is not set then take the first in the whole list
+            prev_article = akorn.articles_container.find('li:first');
+        }
         // Get all the articles that follow the last article before this set
         var latest_articles = prev_article.nextAll();
         // Get the date of the previous article
-        var prev_datetime = prev_article.find('meta').attr('content').substr(0,10);
-        // TODO Remove... just for testing
-        prev_datetime = "qfGQGE";
+        var prev_datetime = prev_article
+                .find('meta')
+                .attr('content')
+                .substr(0,10);
         for(var i=0, len=latest_articles.length; i<len; i++) {
             // Get the date of the article
-            var date_str = $(latest_articles[i]).find('p.meta meta').attr('content').substr(0,10);
+            var date_str = $(latest_articles[i])
+                    .find('p.meta meta')
+                    .attr('content')
+                    .substr(0,10);
             // If the date does not match
             if(date_str !== prev_datetime) {
                 // Make a date line
                 var date_bits = date_str.split('-');
-                $(['<h2>',date_bits[2],' ',akorn.month_names[parseInt(date_bits[1])-1],'</h2>'].join('')).insertBefore(latest_articles[i]);
+                $(['<h2>',date_bits[2],
+                                ' ',
+                                akorn.month_names[parseInt(date_bits[1])-1],
+                                '</h2>']
+                                .join(''))
+                        .insertBefore(latest_articles[i]);
             }
             // Reset the previous date
             prev_datetime = date_str;
         }
     },
+    // Generic throttling function
     throttle: (function () {
-        // Generic throttling function
         return function (fn, delay) {
             delay || (delay = 100);
             var last = +new Date;
@@ -81,6 +128,51 @@ var akorn = {
             return true;
         }
     },
+    search_config: {
+        allowSpaces: true,
+        placeholderText: "Search for journals...",
+        // Called when a tag is added
+        onTagAdded: function(event, tag) {
+	    akorn.get_articles(20,
+                            undefined,
+                            akorn.search_box.tagit("assignedTags").join('+'),
+                            true);
+        },
+        // Called when a tag is removed
+        onTagRemoved: function(event, tag) {
+	    // Get array of assigned tags
+	    var tags_arr = akorn.search_box.tagit("assignedTags");
+	    // Find the removed tag
+	    var tag_idx = $.inArray(tag, tags_arr);
+	    if(tag_idx) {
+                // Remove it
+	        tags_arr.splice(tag_idx, 1);
+	    }
+	    akorn.get_articles(20,
+                            undefined,
+                            tags_arr.join('+'),
+                            true);
+        },
+        // Function to use for auto-completion
+        tagSource: function(search, showChoices) {
+                $.ajax({
+                        url: "/api/journals",
+                        data: {'term': search.term},
+                        dateType: "json",
+                        // TODO Copy and paste code, should be possible to improve
+                        success: function(data) {
+                                var assigned = akorn.search_box.tagit("assignedTags");
+                                var filtered = [];
+                                for (var i=0, dlen=data.length; i < dlen; i++) {
+                                        if ($.inArray(data[i], assigned) == -1) {
+                                                filtered.push(data[i]);
+                                        }
+                                }
+                                showChoices(filtered);
+                        }
+                });
+        },
+    },
     init: function() {
         // Get the place to stick articles
         // Set it as a static property to be accessible across instances
@@ -89,13 +181,8 @@ var akorn = {
         akorn.get_articles(20);
         // TODO Search input should be hidden before this point
         // Activate the search box
-        $('#search').tagsInput({
-            autocomplete_url:'/api/journals',
-            autocomplete:{selectFirst:true,width:'100px',autoFill:true},
-            height: '15px',
-            width: '800px',
-            defaultText: 'Search',
-        });
+        akorn.search_box = $('#search');
+        akorn.search_box.tagit(akorn.search_config);
         // Listen to window scroll events
         // Reduce spurious calls by adding a 250 ms delay between triggers
         $(window).scroll(akorn.throttle(function() {
