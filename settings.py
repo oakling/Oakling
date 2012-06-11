6978a4e8830e2c52cad47401a563888598ea8daf
Craig Loftus <craigloftus+github@gmail.com>
2012-06-11 18:32:53 +0100

    Removing humanize contrib app as it is not used anymore

--- settings.py
+++ settings.py
@@ -121,8 +121,6 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Adding 'human touch' filters
-    'django.contrib.humanize',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
