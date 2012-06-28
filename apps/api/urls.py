d1316e34e0d0f0f01d43f7222c84a1599fa238e6
Craig Loftus <craigloftus@gmail.com>
2012-06-28 21:52:01 +0100

    Adding delete saved search

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -4,5 +4,6 @@ import views
 urlpatterns = patterns('',
     url(r'^latest/(?P<num>[0-9]+)$', views.latest, name='latest'),
     url(r'^journals$', views.journals, name='journals'),
-    url(r'^save_search', views.save_search, name='save_search')
+    url(r'^save_search', views.save_search, name='save_search'),
+    url(r'^remove_search', views.del_saved_search, name='remove_search'),
 )
