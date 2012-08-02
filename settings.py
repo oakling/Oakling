41d85196b345356dd105c591d743a58fa8c462e0
Tim Green <timothy.green@gmail.com>
2012-08-02 20:32:45 +0100

    Links on journal backend page

--- settings.py
+++ settings.py
@@ -126,6 +126,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'apps.search',
+    'apps.api' 
 )
 
 # A sample logging configuration. The only tangible logging
