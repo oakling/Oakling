a178b0723be71dd767bbe21c81a9b6100ba7e857
Tim Green <timothy.green@gmail.com>
2012-09-24 18:45:23 +0100

    Start at proper models for various concepts

--- apps/api/models.py
+++ apps/api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+import couchdb
 
-# Create your models here.
+db_store = couchdb.Server()['store']
+db_journals = couchdb.Server()['journals']
+
+class Article:
+  pass
+
+class Journal:
+  def __init__(self, doc):
+    self.doc = doc
+
+  @classmethod
+  def get_journal(klass, journal_id):
+    doc = db_journals[journal_id]
+    return Journal(doc)
+
+  def get_articles(self, since_time=None, since_docid=None):
+    pass
+
+  def get_num_new(self, since_time=None):
+    pass
+
+class SavedSearch:
+  def get_articles(self, since_time=None, self_docid=None):
+    pass
+
