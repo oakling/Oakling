a39307d3e16311e7494347ba8d221b12c425ee12
Craig Loftus <craigloftus@gmail.com>
2012-10-22 21:15:28 +0100

    Adding first date line

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -17,3 +18,10 @@ urlpatterns = patterns('',
     url(r'^api/', include('apps.api.urls', namespace='api')),
     url(r'^', include('apps.search.urls', namespace='search')),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^'+settings.STATIC_URL[1:]+'(?P<path>.*)$',
+            'django.views.static.serve',
+            {'document_root': settings.STATIC_ROOT}),
+    )
