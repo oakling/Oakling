7aa70a89914756ea1ffba97186965a03921ee91f
Craig Loftus <craigloftus@gmail.com>
2012-10-30 21:20:01 +0000

    Adding initial panes app

--- settings.py
+++ settings.py
@@ -122,11 +122,12 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'apps.search',
-    'apps.api' 
+    'apps.api',
+    'apps.panes'
 )
 
 # A sample logging configuration. The only tangible logging
