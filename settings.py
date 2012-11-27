c5ed17795315fa8b8f8016ab17cb5ecf87ecfbc2
Akorn team <team@akorn.org>
2012-11-27 20:13:42 +0000

    Changing sqlite path

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': './db/akorn.sqlite',                      # Or path to database file if using sqlite3.
+        'NAME': '/home/ubuntu/akorn/akorn_search/db/akorn.sqlite',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
