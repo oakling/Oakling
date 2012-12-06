5e8608947a6e97307800579e1bd6bd13f1dd51ba
Tim Green <timothy.green@gmail.com>
2012-12-06 19:00:15 +0000

    Fix scrape meta tags and couch file when there's no local couch db

--- couch.py
+++ couch.py
@@ -3,7 +3,13 @@ import local_settings
 
 server = couchdb.Server(local_settings.COUCH_SERVER)
 
-db_store = server['store']
-db_journals = server['journals']
-db_scrapers = server['scrapers']
+def get_db(db):
+  try:
+    return server[db]
+  except:
+    return None
+
+db_store = get_db('store')
+db_journals = get_db('journals')
+db_scrapers = get_db('scrapers')
 
