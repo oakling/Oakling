d20262c47cececb430d8bce47bb43e86af7034ea
Craig Loftus <craigloftus@gmail.com>
2013-01-17 20:16:29 +0000

    Adding fab commands for getting and loading couchdb dumps

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,26 @@
 from fabric.api import local, run, cd, sudo, env
 
+import tempfile
+
 env.hosts = ['ubuntu@akorn.org']
 env.directory = "/home/ubuntu/akorn/akorn_search"
+
+def get_dump(output):
+    local('scp {host}:~/dump/fulldump {out}'.format(host=env.hosts[0],
+        out=output))
+
+def load_dump(dump_file=None, couch_url='http://localhost:5984/store/'):
+    # Delete database if already present
+    local('curl -X DELETE {url}'.format(url=couch_url))
+    # Create a new database
+    local('curl -X PUT {url}'.format(url=couch_url))
+    # If a dump file has not been specified then grab one from the server
+    if not dump_file:
+        fd, dump_file = tempfile.mkstemp()
+        get_dump(dump_file)
+    # Load contents into new database
+    local('couchdb-load --input={input} {url}'.format(input=dump_file,
+        url=couch_url))
 
 def prepare_deploy():
     local('git push')
