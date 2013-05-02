036449026bf34ac6693d441c97aa4ca61b7f4f4c
Craig Loftus <craigloftus@gmail.com>
2013-05-02 21:26:34 +0100

    Renaming view names in search app and ensuring all links use url tag

--- akorn_search/apps/search/urls.py
+++ akorn_search/apps/search/urls.py
@@ -3,6 +3,6 @@ import views
 
 urlpatterns = patterns('',
     url(r'^search/$', views.HomeView.as_view(), name='home'),
-    url(r'^doc/(?P<id>[A-Za-z0-9]+)$', views.DocView.as_view(), name='viewdoc'),
-    url(r'^journals/(?P<id>.*)$', views.JournalView.as_view(), name='viewjournal'),
+    url(r'^doc/(?P<id>[A-Za-z0-9]+)$', views.DocView.as_view(), name='doc'),
+    url(r'^journal/(?P<id>.*)$', views.JournalView.as_view(), name='journal'),
 )
