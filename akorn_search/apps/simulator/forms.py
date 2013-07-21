d4ec69547d6bb0562ec58a1fb622355a808c7e75
Craig Loftus <craigloftus@gmail.com>
2013-07-21 18:15:13 +0100

    Adding initial value for scraper config input

--- akorn_search/apps/simulator/forms.py
+++ akorn_search/apps/simulator/forms.py
@@ -1,5 +1,16 @@
 from django import forms
 
+example_config = """<?xml version=\"1.0\"?>
+<scraper>
+    <journal>
+        <title type=\"metaTag\" value=\"\" />
+        <date_published type=\"metaTag\" value=\"\" />
+        <author_names type=\"metaList\" value=\"\" />
+        <journal type=\"metaTag\" value=\"\" />
+        <abstract type=\"css\" value=\"\" />
+    </journal>
+</scraper>"""
+
 class SimulatorForm(forms.Form):
-    scraper_config = forms.CharField(widget=forms.Textarea)
+    scraper_config = forms.CharField(widget=forms.Textarea, initial=example_config)
     article_url = forms.URLField()
