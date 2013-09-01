oakling/Oakling

commit 1393f5f573d93661c1a688724730433c82226da0

Fixing problem with spaces being introduced to lucene queries

akorn_search/apps/api/tests.py
@@ -255,21 +255,22 @@ class ArticlesViewSplitArgTestCase(TestCase):
         self.assertEqual([args], bits)
 
     def test_lucene_multiple(self):
-        args = "Fish+Dog+Horse"
+        delimiter = views.ArticlesView.delimiter
+        args = delimiter.join(["Fish", "Dog", "Horse"])
         bits = views.ArticlesView.lucene_split_arg(args)
         self.assertEqual(["Fish", "Dog", "Horse"], bits)
 
 
 class ArticlesViewQueryTestCase(TestCase):
     def test_make_full_query(self):
-        out = views.ArticlesView.lucene_get_query(keywords=['Word', 'Fish'],
+        out = views.ArticlesView.lucene_get_query(keywords=['Word Horse', 'Fish'],
             journals=['21412412', '1241525211'])
-        expected = "Word AND Fish* AND journalID:(21412412 OR 1241525211)"
+        expected = '("Word Horse" OR "Fish") AND journalID:(21412412 OR 1241525211)'
         self.assertEqual(out, expected)
 
     def test_make_keyword_query(self):
         out = views.ArticlesView.lucene_get_query(keywords=['Word', 'Fish'])
-        expected = "Word AND Fish*"
+        expected = '("Word" OR "Fish")'
         self.assertEqual(out, expected)
 
     def test_make_journal_query(self):

akorn_search/apps/api/views.py
@@ -146,6 +146,7 @@ class ArticlesView(TemplateView):
     template_name = 'search/article_list.html'
     lucene_url = settings.LUCENE_URL
     max_limit = 50
+    delimiter = '|'
 
     @property
     def doc_limit(self):
@@ -183,24 +184,24 @@ class ArticlesView(TemplateView):
     def lucene_process(response):
         return [x['doc'] for x in response['rows']]
 
-    @staticmethod
-    def lucene_split_keywords(arg):
+    @classmethod
+    def lucene_split_keywords(cls, arg):
         try:
             arg = arg.strip()
             if not arg:
                 return []
-            args = arg.split('|')
+            args = arg.split(cls.delimiter)
             return [_.split(' ') for _ in args]
         except AttributeError:
             return []
 
-    @staticmethod
-    def lucene_split_arg(arg):
+    @classmethod
+    def lucene_split_arg(cls, arg):
         try:
             arg = arg.strip()
             if not arg:
                 return []
-            return arg.split('|')
+            return arg.split(cls.delimiter)
         except AttributeError:
             return []
 
@@ -215,7 +216,7 @@ class ArticlesView(TemplateView):
         if keywords:
             # AND between all keywords
             # The last word may not be complete - add a wildcard character
-            keywords_str = "(" + " OR ".join(["\"" + " ".join(_) + "\"" for _ in keywords]) + ")"
+            keywords_str = "(" + " OR ".join(["\"" + _ + "\"" for _ in keywords]) + ")"
         # Deal with the case that there are no journals to be filtered by
         if journals:
            journals_str = ''.join(['journalID:(',' OR '.join(journals),')'])

commit 779bcb2422521922af7530091add90b361bbd245

Fixing regression in keyword parsing introduced in 1393f5f

akorn_search/apps/api/tests.py
@@ -261,15 +261,27 @@ class ArticlesViewSplitArgTestCase(TestCase):
         self.assertEqual(["Fish", "Dog", "Horse"], bits)
 
 
+class ArticlesViewKeywordTestCase(TestCase):
+    def test_lucene_single_keyword_split(self):
+        delimiter = views.ArticlesView.delimiter
+        in_str = "Horse"
+        out_bits = views.ArticlesView.lucene_split_keywords(in_str)
+        self.assertEqual([["Horse"]], out_bits)
+
+    def test_lucene_multiple_keyword_split(self):
+        in_str = "Horse Donkey|Cat|Fish"
+        out_bits = views.ArticlesView.lucene_split_keywords(in_str)
+        self.assertEqual([["Horse", "Donkey"], ["Cat"], ["Fish"]], out_bits)
+
 class ArticlesViewQueryTestCase(TestCase):
     def test_make_full_query(self):
-        out = views.ArticlesView.lucene_get_query(keywords=['Word Horse', 'Fish'],
+        out = views.ArticlesView.lucene_get_query(keywords=[['Word', 'Horse'], ['Fish']],
             journals=['21412412', '1241525211'])
         expected = '("Word Horse" OR "Fish") AND journalID:(21412412 OR 1241525211)'
         self.assertEqual(out, expected)
 
     def test_make_keyword_query(self):
-        out = views.ArticlesView.lucene_get_query(keywords=['Word', 'Fish'])
+        out = views.ArticlesView.lucene_get_query(keywords=[['Word'], ['Fish']])
         expected = '("Word" OR "Fish")'
         self.assertEqual(out, expected)
 
@@ -278,6 +290,14 @@ class ArticlesViewQueryTestCase(TestCase):
             journals=['312452341', '211241412'])
         expected = "journalID:(312452341 OR 211241412)"
         self.assertEqual(out, expected)
+
+    def test_single_keyword_query(self):
+        out = views.ArticlesView.lucene_get_query(keywords=[['Horse']])
+        self.assertEqual(out, '("Horse")')
+
+    def test_single_journal_query(self):
+        out = views.ArticlesView.lucene_get_query(journals=['Horse'])
+        self.assertEqual(out, 'journalID:(Horse)')
 
 
 class ArticlesViewLimitTestCase(TestCase):

akorn_search/apps/api/views.py
@@ -221,7 +221,7 @@ class ArticlesView(TemplateView):
         if keywords:
             # AND between all keywords
             # The last word may not be complete - add a wildcard character
-            keywords_str = "(" + " OR ".join(["\"" + _ + "\"" for _ in keywords]) + ")"
+            keywords_str = "(" + " OR ".join(["\"" + ' '.join(_) + "\"" for _ in keywords]) + ")"
         # Deal with the case that there are no journals to be filtered by
         if journals:
            journals_str = ''.join(['journalID:(',' OR '.join(journals),')'])