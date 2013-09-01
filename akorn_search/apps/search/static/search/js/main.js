oakling/Oakling

commit 103b1d1b817874f445cb464729e7d2e954dd2b2d

Moving to pull in saved searches by get request

akorn_search/apps/search/static/search/js/main.js
@@ -190,13 +190,13 @@ var akorn = {
     },
     // Add the next chunk of articles for the current query
     add_more_articles: function() {
-            var ak = akorn;
-            // Get the last article for use later
-            ak.prev_article = ak.articles_container
-                .find('li:last-child');
-            // Get articles after the current last article
-            var tags = ak.search_box.select2("data");
-            ak.get_articles(tags);
+        var ak = akorn;
+        // Get the last article for use later
+        ak.prev_article = ak.articles_container
+            .find('li:last-child');
+        // Get articles after the current last article
+        var tags = ak.search_box.select2("data");
+        ak.get_articles(tags);
     },
     insert_date_line: function(date_str, latest_article) {
         var content = 'Today', month, day;
@@ -416,11 +416,18 @@ var akorn = {
     },
     post_saved_search: function(query) {
     // Take a given query and save it to the server
-        $.post('/api/save_search', {query: JSON.stringify(query)},
+        $.post('/api/searches', {query: JSON.stringify(query)},
             function(data){
-                console.log('Query saved successfully');
                 akorn.add_saved_search(query, data['query_id']);
             }, 'json');
+    },
+    get_saved_searches: function() {
+        $.getJSON('/api/searches', function(data) {
+            var ak = akorn;
+            for(id in data) {
+                ak.add_saved_search(data[id], id);
+            }
+        });
     },
     save_search_handler: function(e) {
     // Handles clicks on the save this query button
@@ -462,7 +469,6 @@ var akorn = {
         History.replaceState(akorn.state(), "");
     },
     load_state: function(e) {
-        // Set the state using state passed by popstate event
         var state = History.getState();
         // Check for state
         if(state.data === undefined) {
@@ -479,6 +485,8 @@ var akorn = {
         // Get the place to stick articles
         // Set it as a static property to be accessible across instances
         ak.articles_container = $('#articles');
+        // Load saved searches
+        ak.get_saved_searches();
         // Activate search box
         ak.activate_search_box();
 

akorn_search/apps/api/urls.py
@@ -5,7 +5,7 @@ from .views import JournalAutoCompleteView, DeleteSavedSearchView,\
 urlpatterns = patterns('',
     url(r'^journals$', JournalAutoCompleteView.as_view(), name='journals'),
     url(r'^articles$', ArticlesView.as_view(), name='articles'),
-    url(r'^save_search$', SavedSearchView.as_view(), name='save_search'),
+    url(r'^searches$', SavedSearchView.as_view(), name='save_search'),
     url(r'^remove_search$', DeleteSavedSearchView.as_view(), name='remove_search'),
     url(r'^num_new$', ArticleCountView.as_view(), name='num_new'),
 )

akorn_search/apps/api/views.py
@@ -95,6 +95,13 @@ class SavedSearchView(SavedSearchMixin, JSONResponseMixin, View):
         # Return the saved query's id
         return query_id
 
+    def get(self, request, **kwargs):
+        """
+        Return all saved searches
+        """
+        searches, user = self.get_saved_searches()
+        return self.render_to_response(searches)
+
     @method_decorator(csrf_exempt)
     def post(self, request, **kwargs):
         """

akorn_search/apps/search/templates/search/home.html
@@ -27,21 +27,6 @@ $(document).ready(akorn.init);
 <div id="main_row" class="row">
     <div class="span3">
         <ul id="saved_searches">
-            {% for q_id, q in saved_searches.items %}
-                {% spaceless %}
-                <li data-query="{{ q.queries|to_json|escapejs }}" id="{{ q_id }}">
-                <span class="count">
-                    {% if q.count > 0 %}
-                        {{ q.count|query_count }}
-                    {% endif %}
-                </span>
-                <span data-queryid="{{ q_id }}" class="delete">&times;</span>
-                <div class="terms">
-                    <p>{{ q|short_label }}</p>
-                </div>
-                </li>
-                {% endspaceless %}
-            {% endfor %}
         </ul>
     </div>
 

commit 305440dc2d9257d96ea8d2ad95480a63f45df433

Fixing regression in saved search handling introduced in 103b1d1

akorn_search/apps/search/static/search/js/main.js
@@ -410,9 +410,10 @@ var akorn = {
                     '<div class="terms"><p>',
                     ak.shorten_query(query),
                     '</p></div></li>'].join(''));
-        el.children('a').data('query', $.extend(true, {}, query));
         // Add to list of saved searches
         ak.saved_searches.prepend(el);
+        // Add data
+        el.data('query', query);
     },
     post_saved_search: function(query) {
     // Take a given query and save it to the server